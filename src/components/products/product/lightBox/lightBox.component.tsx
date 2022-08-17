ffb416798da008fb719a1ade28e3b178c4cf6547
minde8888 <mindebaltru@gmail.com>
2022-08-17 22:35:41 +0200

    light box

--- src/components/products/product/lightBox/lightBox.component.tsx
+++ src/components/products/product/lightBox/lightBox.component.tsx
@@ -1,18 +1,22 @@
-import { useState } from 'react';
+import { MouseEventHandler, useState } from 'react';
+import style from './lightBox.module.scss'
 
 interface Props {
     images: Array<string> | undefined;
 }
 
+
 const LightBox = (props: Props) => {
-    const [imageToShow, setImageToShow] = useState<string | undefined>('');
-    const [lightBoxDisplay, setLightBoxDisplay] = useState(false);
 
     if (!props.images || props.images.length == 0) return null;
 
+    const [lightBoxDisplay, setLightBoxDisplay] = useState(false);
+    const [newClass, setNewClass] = useState("");
+    const [imageToShow, setImageToShow] = useState<string | undefined>(props.images[0]);
+
     const imageCards: JSX.Element[] = props.images.map((image: string, key: number) => (
         <div key={key} className={'smallImg'}>
-            <img className="image" onClick={() => showImage(key)} src={image} />
+            <img className={style.cursor} onClick={() => showImage(key)} src={image} />
         </div>
     ));
 
@@ -25,9 +29,12 @@ const LightBox = (props: Props) => {
     const hideLightBox = () => {
         setLightBoxDisplay(false);
     };
-    const showLightBox = () => {
-        console.log(1111);
+    const showLightBox = (event: React.MouseEvent<HTMLElement>) => {
+        setNewClass("LightBox");
     };
+    const closeLightBox = () => {
+        setNewClass("");
+    }
     // const showNext = (e: { stopPropagation: () => void; }) => {
     //     e.stopPropagation();
     //     let currentIndex = props.images.indexOf(imageToShow);
@@ -64,8 +71,9 @@ const LightBox = (props: Props) => {
                 </div>
             ) : (
                 <div>
-                    <div className="mainImg">
-                        <img onClick={showLightBox} src={imageToShow} />
+                    <div className={style[newClass]}>
+                        <div className={style.close} style={{ display: (!newClass) ? 'none' : 'block' }} onClick={closeLightBox}>&#x274C;</div>
+                        <img className={style.cursor} onClick={showLightBox} src={imageToShow} />
                     </div>
                     {imageCards}
                 </div>
